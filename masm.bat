8b38c7e2b6b69a15c4cb9907269055a052da66a9
Nikita Isyuk <Nikita.Isyuk@outlook.com>
2022-09-02 16:57:43 +0300

    ok

--- masm.bat
+++ masm.bat
@@ -1,4 +1,5 @@
 @echo off
+if exist %1\%2.exe del %1\%2.exe
 MASM_SDK\ml.exe /c %1\%2.asm
 MASM_SDK\link.exe /DEFAULTLIB:MASM_SDK\kernel32.Lib /DEFAULTLIB:MASM_SDK\User32.Lib /DEFAULTLIB:MASM_SDK\Gdi32.Lib /SUBSYSTEM:WINDOWS %2.obj
 if exist %2.obj del %2.obj
