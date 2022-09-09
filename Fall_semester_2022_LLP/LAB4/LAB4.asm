b96afc0cb4d44d92a5bd29009cbcdffc886426b6
Nikita Isyuk <Nikita.Isyuk@outlook.com>
2022-09-09 22:45:33 +0300

    now lab4 can get console input

--- Fall_semester_2022_LLP/LAB4/LAB4.asm
+++ Fall_semester_2022_LLP/LAB4/LAB4.asm
@@ -8,27 +8,62 @@ option casemap : none
 
 GetStdHandle proto :dword 
 WriteConsoleA proto :dword, :dword, :dword, :dword, :dword
+ReadConsoleA proto :dword, :dword, :dword, :dword, :dword
 ExitProcess proto :dword 
 
 .data
     message db "Hello World!", 0DH, 0AH
-    lmessage equ $ - message
+    ;lmessage equ $ - message
+    lmessage equ 100
 .data?
-    consoleOutHandle dd ? 
-    bytesWritten dd ? 
+    consoleOutHandle dd ?
+    consoleInHandle dd ?
+    bytesWritten dd ?
+    byteArray db ?
 .const
-    STD_OUTPUT_HANDLE equ -11 
+    STD_OUTPUT_HANDLE equ -11
     STD_INPUT_HANDLE equ -10
 
 .code
 lab4:
-    invoke GetStdHandle, STD_OUTPUT_HANDLE
-    mov consoleOutHandle, eax 
-    invoke WriteConsoleA, \
-           consoleOutHandle, \
-           offset message, \
-           lmessage, \
-           offset bytesWritten, \
-           0
+
+    call readFromConsole
+    
+    ; TODO: should found third word in bytesWritten
+    ; TODO: print first symbol of this word
+    ; TODO: print length of this word
+
+    call writeOnConsole
+
     invoke ExitProcess, 0 
+
+    readFromConsole proc
+        pushad
+        invoke GetStdHandle, STD_INPUT_HANDLE
+        mov consoleInHandle, eax
+
+        invoke ReadConsoleA, \
+               consoleInHandle, \
+               offset byteArray, \
+               lmessage, \
+               offset bytesWritten, \
+               0
+        popad
+        ret
+    readFromConsole endp
+
+    writeOnConsole proc
+        pushad
+        invoke GetStdHandle, STD_OUTPUT_HANDLE
+        mov consoleOutHandle, eax
+
+        invoke WriteConsoleA, \
+            consoleOutHandle, \
+            offset byteArray, \
+            lmessage, \
+            offset bytesWritten, \
+            0
+        popad
+        ret
+    writeOnConsole endp
 end lab4
